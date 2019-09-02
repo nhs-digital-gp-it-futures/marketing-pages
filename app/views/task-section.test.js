9ce387fc3aca38c4074c320f55b602a9ea111040
alibombaye <alibombaye@gmail.com>
2019-09-02 16:19:23 +0100

    test to check task list of 1 is rendered

--- app/views/task-section.test.js
+++ app/views/task-section.test.js
@@ -4,13 +4,18 @@ import nunjucks from 'nunjucks';
 import cheerio from 'cheerio';
 import { App } from '../../app';
 
+const aTaskContext = title => ({
+  URL: 'someUrl',
+  title,
+});
 
-const aSectionGroupContext = {
+const aSectionGroupContext = (task = []) => ({
   section: {
     id: 'some-section-group-id',
     name: 'Section Group Title',
+    task,
   },
-};
+});
 
 const createDummyApp = (context) => {
   const app = new App().createApp();
@@ -32,13 +37,28 @@ const createDummyApp = (context) => {
 
 describe('task-section', () => {
   it('should render the title of the section group', (done) => {
-    const dummyApp = createDummyApp(aSectionGroupContext);
+    const dummyApp = createDummyApp(aSectionGroupContext());
     request(dummyApp)
       .get('/')
       .then((res) => {
         const $ = cheerio.load(res.text);
 
         expect($('h2').text().trim()).toEqual('Section Group Title');
+
+        done();
+      });
+  });
+
+  it('should render a task list of 1 if the section group only contains the 1 task', (done) => {
+    const aSectionWithOneTask = aSectionGroupContext([aTaskContext('Some Task')]);
+    const dummyApp = createDummyApp(aSectionWithOneTask);
+
+    request(dummyApp)
+      .get('/')
+      .then((res) => {
+        const $ = cheerio.load(res.text);
+
+        expect($('.app-task-list__item').length).toEqual(1);
 
         done();
       });
