e218cf6d2998d1195466b08d5249a79ba43f1ef1
alibombaye <alibombaye@gmail.com>
2019-09-02 15:52:57 +0100

    added the stylings for task lists

--- app/controller.js
+++ app/controller.js
@@ -1,8 +1,16 @@
 export const getMarketingPageDashboardContext = async () => {
   const context = {
     section: {
-      id: 'some-section-group-id',
-      name: 'Section Group Title',
+      id: 'about-your-solution',
+      name: 'About your Solution',
+      task: [
+        {
+          URL: 'features',
+          title: 'Features',
+          requirement: 'Optional',
+          status: 'complete',
+        },
+      ],
     },
   };
   return context;
