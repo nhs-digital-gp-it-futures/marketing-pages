d23874733930f32c6ce4c9b6e7d9759b4d2bde6e
alibombaye <alibombaye@gmail.com>
2019-09-20 15:43:09 +0100

    refactored createUpdatedSolutionData to use findExistingMarketingDataForTask to find the task to update

--- app/helpers/createUpdatedSolutionData.js
+++ app/helpers/createUpdatedSolutionData.js
@@ -1,8 +1,11 @@
+import { findExistingMarketingDataForTask } from './findExistingMarketingDataForTask';
+
 export const createUpdatedSolutionData = (taskId, existingSolutionData, taskData) => {
   const updatedSolutionData = { ...existingSolutionData };
 
-  updatedSolutionData.marketingData.tasks[0].data = taskData;
-  updatedSolutionData.marketingData.tasks[0].status = 'COMPLETE';
+  const taskToUpdate = findExistingMarketingDataForTask(updatedSolutionData, taskId);
+  taskToUpdate.data = taskData;
+  taskToUpdate.status = 'COMPLETE';
 
   return updatedSolutionData;
 };
