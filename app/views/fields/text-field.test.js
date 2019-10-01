db73c1a131bff23457a5263b20dff07c69242fcf
alibombaye <alibombaye@gmail.com>
2019-10-01 14:35:02 +0100

    added component for text field

---
+++ app/views/fields/text-field.test.js
@@ -0,0 +1,88 @@
+import request from 'supertest';
+import express from 'express';
+import nunjucks from 'nunjucks';
+import cheerio from 'cheerio';
+import { App } from '../../../app';
+
+const createDummyApp = (context) => {
+  const app = new App().createApp();
+
+  const router = express.Router();
+  const dummyRouter = router.get('/', (req, res) => {
+    const macroWrapper = `{% from './fields/text-field.njk' import textField %}
+                            {{ textField(question) }}`;
+
+    const viewToTest = nunjucks.renderString(macroWrapper, context);
+
+    res.send(viewToTest);
+  });
+
+  app.use(dummyRouter);
+
+  return app;
+};
+
+describe('textField', () => {
+  it('should render the main advice', (done) => {
+    const context = {
+      question: {
+        id: 'fieldId',
+        mainAdvice: 'Some really important main advice',
+        additionalAdvice: 'Some not so important additional advice',
+      },
+    };
+
+    const dummyApp = createDummyApp(context);
+    request(dummyApp)
+      .get('/')
+      .then((res) => {
+        const $ = cheerio.load(res.text);
+
+        expect($('label.nhsuk-label').text().trim()).toEqual('Some really important main advice');
+
+        done();
+      });
+  });
+
+  it('should render the additional advice', (done) => {
+    const context = {
+      question: {
+        id: 'fieldId',
+        mainAdvice: 'Some really important main advice',
+        additionalAdvice: 'Some not so important additional advice',
+      },
+    };
+
+    const dummyApp = createDummyApp(context);
+    request(dummyApp)
+      .get('/')
+      .then((res) => {
+        const $ = cheerio.load(res.text);
+
+        expect($('span.nhsuk-hint').text().trim()).toEqual('Some not so important additional advice');
+
+        done();
+      });
+  });
+
+  it('should render the input text field', (done) => {
+    const context = {
+      question: {
+        id: 'fieldId',
+        mainAdvice: 'Some really important main advice',
+        additionalAdvice: 'Some not so important additional advice',
+      },
+    };
+
+    const dummyApp = createDummyApp(context);
+    request(dummyApp)
+      .get('/')
+      .then((res) => {
+        const $ = cheerio.load(res.text);
+
+        expect($('input').length).toEqual(1);
+
+        done();
+      });
+  });
+});
