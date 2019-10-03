be05f1b1f65666d21e4f402d8668f86616416bc0
alibombaye <alibombaye@gmail.com>
2019-10-03 15:21:32 +0100

    created error summary component

---
+++ app/views/section-error-summary.test.js
@@ -0,0 +1,42 @@
+import request from 'supertest';
+import express from 'express';
+import nunjucks from 'nunjucks';
+import cheerio from 'cheerio';
+import { App } from '../../app';
+
+const createDummyApp = (context) => {
+  const app = new App().createApp();
+
+  const router = express.Router();
+  const dummyRouter = router.get('/', (req, res) => {
+    const macroWrapper = `{% from './section-error-summary.njk' import sectionErrorSummary %}
+                            {{ sectionErrorSummary(errors) }}`;
+
+    const viewToTest = nunjucks.renderString(macroWrapper, context);
+
+    res.send(viewToTest);
+  });
+
+  app.use(dummyRouter);
+
+  return app;
+};
+
+describe('sectionErrorSummary', () => {
+  it('should render the error summary title', (done) => {
+    const context = {
+      errors: [],
+    };
+
+    const dummyApp = createDummyApp(context);
+    request(dummyApp)
+      .get('/')
+      .then((res) => {
+        const $ = cheerio.load(res.text);
+
+        expect($('.nhsuk-error-summary h2').text().trim()).toEqual('There is a problem');
+
+        done();
+      });
+  });
+});
