6bbe803abc72218acb2320cf65b11083879bf671
alibombaye <alibombaye@gmail.com>
2019-10-09 13:15:21 +0100

    Added preview page and shown title

---
+++ app/views/preview-page.test.js
@@ -0,0 +1,34 @@
+import request from 'supertest';
+import express from 'express';
+import cheerio from 'cheerio';
+import { App } from '../../app';
+
+const createDummyApp = (context) => {
+  const app = new App().createApp();
+
+  const router = express.Router();
+  const dummyRouter = router.get('/', (req, res) => {
+    res.render('preview-page.njk', context);
+  });
+
+  app.use(dummyRouter);
+
+  return app;
+};
+
+describe('preview page', () => {
+  it('should render the title of the preview page', (done) => {
+    const context = {};
+
+    const dummyApp = createDummyApp(context);
+    request(dummyApp)
+      .get('/')
+      .then((res) => {
+        const $ = cheerio.load(res.text);
+
+        expect($('h1').text().trim()).toEqual('Preview Page');
+
+        done();
+      });
+  });
+});
