aa94cc91aced69093b23cec2b617bf50b2494d6e
alibombaye <alibombaye@gmail.com>
2019-10-10 11:41:09 +0100

    Added first preview section template

---
+++ app/views/preview-section.test.js
@@ -0,0 +1,44 @@
+import request from 'supertest';
+import express from 'express';
+import nunjucks from 'nunjucks';
+import cheerio from 'cheerio';
+import { App } from '../../app';
+
+const createDummyApp = (context) => {
+  const app = new App().createApp();
+
+  const router = express.Router();
+  const dummyRouter = router.get('/', (req, res) => {
+    const macroWrapper = `{% from './preview-section.njk' import previewSection %}
+                            {{ previewSection(section) }}`;
+
+    const viewToTest = nunjucks.renderString(macroWrapper, context);
+
+    res.send(viewToTest);
+  });
+
+  app.use(dummyRouter);
+
+  return app;
+};
+
+describe('preview-section', () => {
+  it('should render the title of the section', (done) => {
+    const context = {
+      section: {
+        title: 'Some Section Title',
+      },
+    };
+
+    const dummyApp = createDummyApp(context);
+    request(dummyApp)
+      .get('/')
+      .then((res) => {
+        const $ = cheerio.load(res.text);
+
+        expect($('h2').text().trim()).toEqual('Some Section Title');
+
+        done();
+      });
+  });
+});
