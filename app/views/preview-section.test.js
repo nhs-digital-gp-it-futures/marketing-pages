cada961a8cc7a1d9938792bdc9ae65ec9bbf7fdf
alibombaye <alibombaye@gmail.com>
2019-10-10 11:59:43 +0100

    added preview of question in preview section

--- app/views/preview-section.test.js
+++ app/views/preview-section.test.js
@@ -26,7 +26,7 @@ describe('preview-section', () => {
   it('should render the title of the section', (done) => {
     const context = {
       section: {
-        title: 'Some Section Title',
+        title: 'Some section title',
       },
     };
 
@@ -36,7 +36,35 @@ describe('preview-section', () => {
       .then((res) => {
         const $ = cheerio.load(res.text);
 
-        expect($('h2').text().trim()).toEqual('Some Section Title');
+        expect($('h2').text().trim()).toEqual('Some section title');
+
+        done();
+      });
+  });
+
+  it('should render the question of the section', (done) => {
+    const context = {
+      section: {
+        title: 'Some section title',
+        question: {
+          title: 'Some question title',
+          data: 'Some question data',
+        },
+      },
+    };
+
+    const dummyApp = createDummyApp(context);
+    request(dummyApp)
+      .get('/')
+      .then((res) => {
+        const $ = cheerio.load(res.text);
+
+        const sectionQuestion = $('[data-test-id="preview-section-question"]');
+
+        expect(sectionQuestion.find());
+
+        expect(sectionQuestion.find('[data-test-id="preview-section-question-title"]').text().trim()).toEqual('Some question title');
+        expect(sectionQuestion.find('[data-test-id="preview-section-question-data"]').text().trim()).toEqual('Some question data');
 
         done();
       });
