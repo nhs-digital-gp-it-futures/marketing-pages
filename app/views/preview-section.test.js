e8d809f586cdbf8553b89d9de4c30993a1acd273
alibombaye <alibombaye@gmail.com>
2019-10-10 12:13:31 +0100

    check multiple questions are rendered

--- app/views/preview-section.test.js
+++ app/views/preview-section.test.js
@@ -42,14 +42,17 @@ describe('preview-section', () => {
       });
   });
 
-  it('should render the question of the section', (done) => {
+  it('should render a single question when the section has only one question', (done) => {
     const context = {
       section: {
         title: 'Some section title',
-        question: {
-          title: 'Some question title',
-          data: 'Some question data',
-        },
+        questions: [
+          {
+            id: 'some-question-id',
+            title: 'Some question title',
+            data: 'Some question data',
+          },
+        ],
       },
     };
 
@@ -59,12 +62,49 @@ describe('preview-section', () => {
       .then((res) => {
         const $ = cheerio.load(res.text);
 
-        const sectionQuestion = $('[data-test-id="preview-section-question"]');
-
-        expect(sectionQuestion.find());
+        const sectionQuestion = $('[data-test-id="preview-section-question-some-question-id"]');
 
         expect(sectionQuestion.find('[data-test-id="preview-section-question-title"]').text().trim()).toEqual('Some question title');
         expect(sectionQuestion.find('[data-test-id="preview-section-question-data"]').text().trim()).toEqual('Some question data');
+
+        done();
+      });
+  });
+
+  it('should render a multiple question when the section has multiple questions', (done) => {
+    const context = {
+      section: {
+        title: 'Some section title',
+        questions: [
+          {
+            id: 'some-first-question-id',
+            title: 'Some first question title',
+            data: 'Some first question data',
+          },
+          {
+            id: 'some-second-question-id',
+            title: 'Some second question title',
+            data: 'Some second question data',
+          },
+        ],
+      },
+    };
+
+    const dummyApp = createDummyApp(context);
+    request(dummyApp)
+      .get('/')
+      .then((res) => {
+        const $ = cheerio.load(res.text);
+
+        const firstSectionQuestion = $('[data-test-id="preview-section-question-some-first-question-id"]');
+        const secondSectionQuestion = $('[data-test-id="preview-section-question-some-second-question-id"]');
+
+        expect(firstSectionQuestion.find('[data-test-id="preview-section-question-title"]').text().trim()).toEqual('Some first question title');
+        expect(firstSectionQuestion.find('[data-test-id="preview-section-question-data"]').text().trim()).toEqual('Some first question data');
+
+        expect(secondSectionQuestion.find('[data-test-id="preview-section-question-title"]').text().trim()).toEqual('Some second question title');
+        expect(secondSectionQuestion.find('[data-test-id="preview-section-question-data"]').text().trim()).toEqual('Some second question data');
+
 
         done();
       });
