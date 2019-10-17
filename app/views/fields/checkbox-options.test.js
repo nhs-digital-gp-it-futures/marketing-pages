9538edcefa1db0487073d763f41942ac9fc678b4
alibombaye <alibombaye@gmail.com>
2019-10-17 16:23:38 +0100

    created new component for checkbox options

---
+++ app/views/fields/checkbox-options.test.js
@@ -0,0 +1,67 @@
+import request from 'supertest';
+import express from 'express';
+import nunjucks from 'nunjucks';
+import cheerio from 'cheerio';
+import { App } from '../../../app';
+
+const createDummyApp = (context) => {
+  const app = new App().createApp();
+
+  const router = express.Router();
+  const dummyRouter = router.get('/', (req, res) => {
+    const macroWrapper = `{% from './fields/checkbox-options.njk' import checkboxOptions %}
+                            {{ checkboxOptions(question) }}`;
+
+    const viewToTest = nunjucks.renderString(macroWrapper, context);
+
+    res.send(viewToTest);
+  });
+
+  app.use(dummyRouter);
+
+  return app;
+};
+
+describe('checkboxOptions', () => {
+  it('should render the main advice', (done) => {
+    const context = {
+      question: {
+        id: 'fieldId',
+        mainAdvice: 'Some really important main advice',
+        additionalAdvice: 'Some not so important additional advice',
+      },
+    };
+
+    const dummyApp = createDummyApp(context);
+    request(dummyApp)
+      .get('/')
+      .then((res) => {
+        const $ = cheerio.load(res.text);
+
+        expect($('.nhsuk-fieldset__legend').text().trim()).toEqual('Some really important main advice');
+
+        done();
+      });
+  });
+
+  it('should render the additional advice', (done) => {
+    const context = {
+      question: {
+        id: 'fieldId',
+        mainAdvice: 'Some really important main advice',
+        additionalAdvice: 'Some not so important additional advice',
+      },
+    };
+
+    const dummyApp = createDummyApp(context);
+    request(dummyApp)
+      .get('/')
+      .then((res) => {
+        const $ = cheerio.load(res.text);
+
+        expect($('.nhsuk-hint').text().trim()).toEqual('Some not so important additional advice');
+
+        done();
+      });
+  });
+});
