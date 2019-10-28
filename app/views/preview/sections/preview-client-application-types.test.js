1bdf13272cce4a6691fd5d6816312a489ebe1b50
alibombaye <alibombaye@gmail.com>
2019-10-28 10:32:41 +0000

    created component for preview client application types and removed un used components

---
+++ app/views/preview/sections/preview-client-application-types.test.js
@@ -0,0 +1,40 @@
+import request from 'supertest';
+import express from 'express';
+import nunjucks from 'nunjucks';
+import cheerio from 'cheerio';
+import { App } from '../../../../app';
+
+const createDummyApp = (context) => {
+  const app = new App().createApp();
+
+  const router = express.Router();
+  const dummyRouter = router.get('/', (req, res) => {
+    const macroWrapper = `{% from './preview/sections/preview-client-application-types.njk' import previewClientApplicationTypes %}
+                            {{ previewClientApplicationTypes(section) }}`;
+
+    const viewToTest = nunjucks.renderString(macroWrapper, context);
+
+    res.send(viewToTest);
+  });
+
+  app.use(dummyRouter);
+
+  return app;
+};
+
+describe('preview-client-application-types', () => {
+  it('should render the title of the section', (done) => {
+    const context = {};
+
+    const dummyApp = createDummyApp(context);
+    request(dummyApp)
+      .get('/')
+      .then((res) => {
+        const $ = cheerio.load(res.text);
+
+        expect($('h3').text().trim()).toEqual('Client application type');
+
+        done();
+      });
+  });
+});
