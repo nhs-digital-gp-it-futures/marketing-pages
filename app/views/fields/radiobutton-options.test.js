bb7100007aedca7e270f2067e212b980065d4578
Sam Roberts <53512773+sam-roberts-bjss@users.noreply.github.com>
2019-11-05 10:54:07 +0000

    Add error message to radio button option component

--- app/views/fields/radiobutton-options.test.js
+++ app/views/fields/radiobutton-options.test.js
@@ -133,4 +133,39 @@ describe('radiobuttonOptions', () => {
         done();
       });
   });
+
+  it('should render the radiobutton option as an error if the context provided contains an error', (done) => {
+    const context = {
+      question: {
+        id: 'fieldId',
+        mainAdvice: 'Some really important main advice',
+        additionalAdvice: 'Some not so important additional advice',
+        options: [
+          {
+            value: 'first-option',
+            text: 'First Option',
+          },
+          {
+            value: 'second-option',
+            text: 'Second Option',
+          },
+        ],
+        error: {
+          message: 'Some error message',
+        },
+      },
+    };
+
+    const dummyApp = createDummyApp(context);
+    request(dummyApp)
+      .get('/')
+      .then((res) => {
+        const $ = cheerio.load(res.text);
+
+        expect($('.nhsuk-error-message').text().trim()).toEqual('Error: Some error message');
+        expect($('#fieldId-error').length).toEqual(1);
+
+        done();
+      });
+  });
 });
