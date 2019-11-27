0fc6cf5a491ee4ff75615ab5bdf6328a4c2712a7
kat1906 <katie.armstrong@bjss.com>
2019-11-27 16:05:01 +0000

    fix npm install

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,6 +9,19 @@ module.exports = {
   "rules": {
     "import/prefer-default-export": "off",
     "array-callback-return": "off",
-    "newline-per-chained-call": "off",
-  }
+    "linebreak-style": "off",
+    "no-console": 1,
+  },
+  "globals": {
+    "fixture": "readonly"
+  },
+  "overrides": [
+    {
+      "files": ["**/*ui.test.js"],
+        "rules": {
+          "newline-per-chained-call": 0
+        }
+      }
+    ]
 };
+
