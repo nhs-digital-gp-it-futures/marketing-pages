93a6163fa8dab6d1f1155802d259bea846096548
alibombaye <alibombaye@gmail.com>
2019-12-02 10:41:14 +0000

    added unit test for commonQuestionContext

---
+++ app/section/helpers/commonQuestionContext.test.js
@@ -0,0 +1,47 @@
+import { commonQuestionContext } from './commonQuestionContext';
+
+describe('commonQuestionContext', () => {
+  it('should create the common context for a question', () => {
+    const expectedContext = {
+      id: 'some-question-id',
+      type: 'text-field',
+      mainAdvice: 'some main advice',
+      additionalAdvice: 'some additional advice',
+      footerAdvice: 'some footer advice',
+    };
+
+    const questionManifest = {
+      type: 'text-field',
+      mainAdvice: 'some main advice',
+      additionalAdvice: 'some additional advice',
+      footerAdvice: 'some footer advice',
+    };
+
+
+    const context = commonQuestionContext('some-question-id', questionManifest);
+
+    expect(context).toEqual(expectedContext);
+  });
+
+  it('should create a context for an inner question', () => {
+    const expectedContext = {
+      id: 'parentQuestionId[some-question-id]',
+      type: 'text-field',
+      mainAdvice: 'some main advice',
+      additionalAdvice: 'some additional advice',
+      footerAdvice: 'some footer advice',
+    };
+
+    const questionManifest = {
+      type: 'text-field',
+      mainAdvice: 'some main advice',
+      additionalAdvice: 'some additional advice',
+      footerAdvice: 'some footer advice',
+    };
+
+
+    const context = commonQuestionContext('some-question-id', questionManifest, 'parentQuestionId');
+
+    expect(context).toEqual(expectedContext);
+  });
+});
