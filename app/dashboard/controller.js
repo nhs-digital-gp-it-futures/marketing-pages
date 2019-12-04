d7908b05ece5b1250c941e853ecf86ea3e934035
alibombaye <alibombaye@gmail.com>
2019-12-04 13:05:43 +0000

    added env var and stop bots and update preview template

--- app/dashboard/controller.js
+++ app/dashboard/controller.js
@@ -1,11 +1,12 @@
 import axios from 'axios';
 import { ManifestProvider } from '../manifestProvider';
 import { createDashboardPageContext } from './createDashboardPageContext';
+import { apiHost } from '../config';
 
 export const getMarketingPageDashboardContext = async (solutionId, validationErrors) => {
   const dashboardManifest = new ManifestProvider().getDashboardManifest();
 
-  const dashboardDataRaw = await axios.get(`http://localhost:8080/api/v1/Solutions/${solutionId}/dashboard`);
+  const dashboardDataRaw = await axios.get(`${apiHost}/Solutions/${solutionId}/dashboard`);
   const dashboardData = dashboardDataRaw.data;
 
   const context = createDashboardPageContext(
