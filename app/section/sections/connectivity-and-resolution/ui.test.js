0d437ded8a1c2352b45be406311321b913be4082
alibombaye <alibombaye@gmail.com>
2019-12-12 10:35:44 +0000

    ui tests for validation

--- app/section/sections/connectivity-and-resolution/ui.test.js
+++ app/section/sections/connectivity-and-resolution/ui.test.js
@@ -3,7 +3,6 @@ import { Selector, ClientFunction } from 'testcafe';
 import { ManifestProvider } from '../../../manifestProvider';
 import dashboardWithCompleteSections from '../../../../fixtures/dashboardWithCompleteSections.json';
 
-
 const connectivityAndResolutionMarketingData = {
   'minimum-connection-speed': '1Mbps',
   'minimum-desktop-resolution': '16:9 - 2048 x 1152',
@@ -103,16 +102,80 @@ test('should populate the questions with existing data', async (t) => {
   pageSetup(t, true);
 
   const minimumConnectionSpeedQuestion = Selector('[data-test-id="combobox-options-minimum-connection-speed"]');
-
-  await t
-    .expect(minimumConnectionSpeedQuestion.find('option[selected]').exists).ok()
-    .expect(minimumConnectionSpeedQuestion.find('option[selected]').getAttribute('value')).eql('1Mbps');
-
   const minimumDesktopResolutionQuestion = Selector('[data-test-id="combobox-options-minimum-desktop-resolution"]');
 
   await t
+    .expect(minimumConnectionSpeedQuestion.find('option[selected]').exists).ok()
+    .expect(minimumConnectionSpeedQuestion.find('option[selected]').getAttribute('value')).eql('1Mbps')
+
     .expect(minimumDesktopResolutionQuestion.find('option[selected]').exists).ok()
     .expect(minimumDesktopResolutionQuestion.find('option[selected]').getAttribute('value')).eql('16:9 - 2048 x 1152');
+});
+
+test('should show error summary and validation for minimum connection speed question indicating it is mandatory', async (t) => {
+  await pageSetup(t);
+
+  nock('http://localhost:8080')
+    .put('/api/v1/Solutions/S100000-001/sections/connectivity-and-resolution')
+    .reply(400, {
+      required: ['minimum-connection-speed'],
+    });
+
+  const errorSummary = Selector('[data-test-id="error-summary"]');
+  const errorSummaryList = Selector('.nhsuk-error-summary__list');
+  const minimumConnectionSpeedQuestion = Selector('[data-test-id="combobox-options-minimum-connection-speed"]');
+  const submitButton = Selector('[data-test-id="section-submit-button"]');
+
+  await t
+    .expect(errorSummary.exists).notOk()
+    .click(submitButton.find('button'))
+    .expect(errorSummary.exists).ok()
+    .expect(errorSummaryList.find('li').count).eql(1)
+    .expect(errorSummaryList.find('li:nth-child(1)').innerText).eql('Please select the minimum connection speed required')
+    .expect(errorSummaryList.find('li:nth-child(1) a').getAttribute('href')).eql('#minimum-connection-speed')
+
+    .expect(minimumConnectionSpeedQuestion.find('[data-test-id="combobox-options-error"]').exists).ok()
+    .expect(minimumConnectionSpeedQuestion.find('.nhsuk-error-message').innerText).eql('Error:\nPlease select the minimum connection speed required');
+});
+
+test('should goto anchor when clicking the minimum connection speed summary error link', async (t) => {
+  await pageSetup(t);
+
+  nock('http://localhost:8080')
+    .put('/api/v1/Solutions/S100000-001/sections/connectivity-and-resolution')
+    .reply(400, {
+      required: ['minimum-connection-speed'],
+    });
+
+  const errorSummaryList = Selector('.nhsuk-error-summary__list');
+  const submitButton = Selector('[data-test-id="section-submit-button"]');
+
+  const getLocation = ClientFunction(() => document.location.href);
+
+  await t
+    .click(submitButton.find('button'))
+    .click(errorSummaryList.find('li:nth-child(1) a'))
+    .expect(getLocation()).contains('/S100000-001/section/connectivity-and-resolution#minimum-connection-speed');
+});
+
+test('should goto the browser based dashboard when clicking the submit button', async (t) => {
+  await pageSetup(t, 200, connectivityAndResolutionMarketingData);
+
+  nock('http://localhost:8080')
+    .put('/api/v1/Solutions/S100000-001/sections/connectivity-and-resolution')
+    .reply(200, connectivityAndResolutionMarketingData);
+
+  nock('http://localhost:8080')
+    .get('/api/v1/Solutions/S100000-001/sections/browser-based')
+    .reply(200, {});
+
+  const submitButton = Selector('[data-test-id="section-submit-button"] button');
+  const getLocation = ClientFunction(() => document.location.href);
+
+  await t
+    .expect(submitButton.exists).ok()
+    .click(submitButton)
+    .expect(getLocation()).contains('/S100000-001/dashboard/browser-based');
 });
 
 test('should render the return to all sections link', async (t) => {
