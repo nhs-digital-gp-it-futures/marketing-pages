8ecb572824058c5f1c72c43d976a9d721adc2da6
Ben.Hayward <ben.hayward@bjss.com>
2019-12-19 16:59:54 +0000

    Added validation UI tests

--- app/section/sections/mobile-memory-and-storage/ui.test.js
+++ app/section/sections/mobile-memory-and-storage/ui.test.js
@@ -2,16 +2,16 @@ import nock from 'nock';
 import { Selector, ClientFunction } from 'testcafe';
 import dashboardWithCompleteSections from '../../../../fixtures/dashboardWithCompleteSections.json';
 
-const mobileConnectionData = {
+const mobileMemoryAndStorageData = {
   "minimum-memory-requirement": "256MB",
-  "storage-requirements-description": "Text"
+  "storage-requirements-description": "Some storage requirements description"
 };
 
 const mocks = (withMarketingData) => {
   if (withMarketingData) {
     nock('http://localhost:8080')
       .get('/api/v1/Solutions/S100000-001/sections/mobile-memory-and-storage')
-      .reply(200, mobileConnectionData);
+      .reply(200, mobileMemoryAndStorageData);
   } else {
     nock('http://localhost:8080')
       .get('/api/v1/Solutions/S100000-001/sections/mobile-memory-and-storage')
@@ -24,7 +24,7 @@ const pageSetup = async (t, withMarketingData = false) => {
   await t.navigateTo('http://localhost:1234/solution/S100000-001/section/mobile-memory-and-storage');
 };
 
-fixture.only('Mobile memory and storage page')
+fixture('Mobile memory and storage page')
   .afterEach(async (t) => {
     const isDone = nock.isDone();
     if (!isDone) {
@@ -63,44 +63,124 @@ test('should render all the advice of the section', async (t) => {
 test('should render minimum memory requirement question', async (t) => {
   pageSetup(t);
 
-  const minimumConnectionSpeedQuestion = Selector('[data-test-id="combobox-options-minimum-memory-requirement"]');
+  const minimumMemoryRequirementQuestion = Selector('[data-test-id="combobox-options-minimum-memory-requirement"]');
 
   await t
-    .expect(minimumConnectionSpeedQuestion.find('.nhsuk-label').innerText).eql('Select the minimum memory requirement for your Solution to function?')
-    .expect(minimumConnectionSpeedQuestion.find('.nhsuk-hint').innerText).eql('Please select below the minimum memory requirement.')
-    .expect(minimumConnectionSpeedQuestion.find('select').exists).ok()
-    .expect(minimumConnectionSpeedQuestion.find('option').count).eql(8);
+    .expect(minimumMemoryRequirementQuestion.find('.nhsuk-label').innerText).eql('Select the minimum memory requirement for your Solution to function?')
+    .expect(minimumMemoryRequirementQuestion.find('.nhsuk-hint').innerText).eql('Please select below the minimum memory requirement.')
+    .expect(minimumMemoryRequirementQuestion.find('select').exists).ok()
+    .expect(minimumMemoryRequirementQuestion.find('option').count).eql(8);
 });
 
-test('should populate connection speed question with existing data', async (t) => {
+test('should populate minimum memory requirement question with existing data', async (t) => {
   pageSetup(t, true);
 
-  const minimumConnectionSpeedQuestion = Selector('[data-test-id="combobox-options-minimum-memory-requirement"]');
+  const minimumMemoryRequirementQuestion = Selector('[data-test-id="combobox-options-minimum-memory-requirement"]');
 
   await t
-    .expect(minimumConnectionSpeedQuestion.find('option[selected]').exists).ok()
-    .expect(minimumConnectionSpeedQuestion.find('option[selected]').getAttribute('value')).eql('256MB')
+    .expect(minimumMemoryRequirementQuestion.find('option[selected]').exists).ok()
+    .expect(minimumMemoryRequirementQuestion.find('option[selected]').getAttribute('value')).eql('256MB')
+});
+
+test('should show error summary and validation for minimum memory requirement indicating it is mandatory', async (t) => {
+  await pageSetup(t);
+
+  nock('http://localhost:8080')
+    .put('/api/v1/Solutions/S100000-001/sections/mobile-memory-and-storage')
+    .reply(400, {
+      required: ['minimum-memory-requirement'],
+    });
+
+  const errorSummary = Selector('[data-test-id="error-summary"]');
+  const errorSummaryList = Selector('.nhsuk-error-summary__list');
+  const minimumMemoryRequirement = Selector('[data-test-id="combobox-options-minimum-memory-requirement"]');
+  const submitButton = Selector('[data-test-id="section-submit-button"]');
+
+  await t
+    .expect(errorSummary.exists).notOk()
+    .click(submitButton.find('button'))
+    .expect(errorSummary.exists).ok()
+    .expect(errorSummaryList.find('li').count).eql(1)
+    .expect(errorSummaryList.find('li:nth-child(1)').innerText).eql('Minimum memory requirement is a mandatory question')
+    .expect(errorSummaryList.find('li:nth-child(1) a').getAttribute('href')).eql('#minimum-memory-requirement')
+    .expect(minimumMemoryRequirement.find('[data-test-id="combobox-options-error"]').exists).ok()
+    .expect(minimumMemoryRequirement.find('.nhsuk-error-message').innerText).eql('Error:\nMinimum memory requirement is a mandatory question');
 });
 
 test('should render storage requirements description question', async (t) => {
   await pageSetup(t);
 
-  const connectionRequirementDescription = Selector('[data-test-id="question-storage-requirements-description"]');
+  const storageRequirementsDescription = Selector('[data-test-id="question-storage-requirements-description"]');
 
   await t
-    .expect(connectionRequirementDescription.find('label.nhsuk-label').innerText).eql('Description of storage requirements')
-    .expect(connectionRequirementDescription.find('span.nhsuk-hint').innerText).eql('Add further information about the storage requirements needed for your Solution to function.')
-    .expect(connectionRequirementDescription.find('textarea').count).eql(1)
-    .expect(connectionRequirementDescription.find('[data-test-id="textarea-field-footer"]').innerText).eql('You can enter up to 300 characters');
+    .expect(storageRequirementsDescription.find('label.nhsuk-label').innerText).eql('Description of storage requirements')
+    .expect(storageRequirementsDescription.find('span.nhsuk-hint').innerText).eql('Add further information about the storage requirements needed for your Solution to function.')
+    .expect(storageRequirementsDescription.find('textarea').count).eql(1)
+    .expect(storageRequirementsDescription.find('[data-test-id="textarea-field-footer"]').innerText).eql('You can enter up to 300 characters');
 });
 
-test('should populate the text fields with existing data', async (t) => {
+test('should populate the storage requirements description with existing data', async (t) => {
   pageSetup(t, true);
 
   const theQuestions = Selector('form');
 
   await t
-    .expect(theQuestions.find('[data-test-id="question-storage-requirements-description"]').find('textarea').value).eql('Text');
+    .expect(theQuestions.find('[data-test-id="question-storage-requirements-description"]').find('textarea').value).eql('Some storage requirements description');
+});
+
+test('should show error summary and validation for storage requirements description indicating it is mandatory', async (t) => {
+  await pageSetup(t);
+
+  nock('http://localhost:8080')
+    .put('/api/v1/Solutions/S100000-001/sections/mobile-memory-and-storage')
+    .reply(400, {
+      required: ['storage-requirements-description'],
+    });
+
+  const errorSummary = Selector('[data-test-id="error-summary"]');
+  const errorSummaryList = Selector('.nhsuk-error-summary__list');
+  const storageRequirementsDescription = Selector('[data-test-id="question-storage-requirements-description"]');
+  const submitButton = Selector('[data-test-id="section-submit-button"]');
+
+  await t
+    .expect(errorSummary.exists).notOk()
+    .click(submitButton.find('button'))
+    .expect(errorSummary.exists).ok()
+    .expect(errorSummaryList.find('li').count).eql(1)
+    .expect(errorSummaryList.find('li:nth-child(1)').innerText).eql('Storage requirement description is a mandatory question')
+    .expect(errorSummaryList.find('li:nth-child(1) a').getAttribute('href')).eql('#storage-requirements-description')
+    .expect(storageRequirementsDescription.find('.nhsuk-textarea--error').exists).ok()
+    .expect(storageRequirementsDescription.find('.nhsuk-error-message').innerText).eql('Error:\nStorage requirement description is a mandatory question');
+});
+
+test('should show error summary and validation for storage requirements description question it exceeds the maxLength', async (t) => {
+  await pageSetup(t);
+
+  nock('http://localhost:8080')
+    .put('/api/v1/Solutions/S100000-001/sections/mobile-memory-and-storage')
+    .reply(400, {
+      maxLength: ['storage-requirements-description'],
+    });
+
+  const oneHundredCharacters = '0123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789';
+  const threeHundredCharacters = oneHundredCharacters.repeat(3);
+
+  const errorSummary = Selector('[data-test-id="error-summary"]');
+  const errorSummaryList = Selector('.nhsuk-error-summary__list');
+  const storageRequirementsDescription = Selector('[data-test-id="question-storage-requirements-description"]');
+
+  const submitButton = Selector('[data-test-id="section-submit-button"]');
+
+  await t
+    .expect(errorSummary.exists).notOk()
+    .typeText(storageRequirementsDescription, `${threeHundredCharacters}0`, { paste: true })
+    .click(submitButton.find('button'))
+    .expect(errorSummary.exists).ok()
+    .expect(errorSummaryList.find('li').count).eql(1)
+    .expect(errorSummaryList.find('li:nth-child(1)').innerText).eql('Storage requirement description is over the character limit')
+    .expect(errorSummaryList.find('li:nth-child(1) a').getAttribute('href')).eql('#storage-requirements-description')
+    .expect(storageRequirementsDescription.find('.nhsuk-textarea--error').exists).ok()
+    .expect(storageRequirementsDescription.find('.nhsuk-error-message').innerText).eql('Error:\nStorage requirement description is over the character limit')
 });
 
 test('should render the submit button', async (t) => {
