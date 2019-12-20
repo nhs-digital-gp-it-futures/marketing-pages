855a611af2542661ceb0d0132c130cc2c46084f7
kat1906 <katie.armstrong@bjss.com>
2019-12-20 14:56:07 +0000

    remove spaces

--- app/manifestProvider.js
+++ app/manifestProvider.js
@@ -3,17 +3,13 @@ import fs from 'fs';
 export class ManifestProvider {
   getDashboardManifest() {
     this.dashboardManifestRaw = fs.readFileSync('./app/dashboard/manifest.json');
-
     this.dashboardManifest = JSON.parse(this.dashboardManifestRaw);
-
     return this.dashboardManifest;
   }
 
   getSubDashboardManifest(dashboardId) {
     this.subDashboardManifestRaw = fs.readFileSync(`./app/dashboard/subDashboards/${dashboardId}/manifest.json`);
-
     this.subDashboardManifest = JSON.parse(this.subDashboardManifestRaw);
-
     return this.subDashboardManifest;
   }
 
@@ -22,11 +18,8 @@ export class ManifestProvider {
     sectionId,
   }) {
     const pathToSectionManifest = dashboardId ? `${dashboardId}/${sectionId}` : `${sectionId}`;
-
     this.sectionManifestRaw = fs.readFileSync(`./app/section/sections/${pathToSectionManifest}/manifest.json`);
-
     this.sectionManifest = JSON.parse(this.sectionManifestRaw);
-
     return this.sectionManifest;
   }
 }
