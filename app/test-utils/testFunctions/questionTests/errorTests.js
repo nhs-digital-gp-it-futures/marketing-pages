7420c893b6040666d76023a5e1632ac7248c46bf
kat1906 <katie.armstrong@bjss.com>
2019-12-20 14:31:45 +0000

    move checks into functions

--- app/test-utils/testFunctions/questionTests/errorTests.js
+++ app/test-utils/testFunctions/questionTests/errorTests.js
@@ -45,36 +45,39 @@ const maxLengthErrorTest = ({
   questionId,
   sectionApiUrl,
   apiLocalhost,
+  errorType,
 }) => {
-  test(`should show error summary and validation for ${questionId} question when it exceeds the maxLength`, async (t) => {
-    const questionType = sectionManifest.questions[questionId].type;
-    await pageSetup({ t });
-    nock(apiLocalhost)
-      .put(sectionApiUrl)
-      .reply(400, {
-        // bulletpoint-list numbers the input fields so it is not just the question id
-        maxLength: [questionType === 'bulletpoint-list' ? `${questionId}-1` : questionId],
-      });
+  if (errorType === 'maxLength') {
+    test(`should show error summary and validation for ${questionId} question when it exceeds the maxLength`, async (t) => {
+      const questionType = sectionManifest.questions[questionId].type;
+      await pageSetup({ t });
+      nock(apiLocalhost)
+        .put(sectionApiUrl)
+        .reply(400, {
+          // bulletpoint-list numbers the input fields so it is not just the question id
+          maxLength: [questionType === 'bulletpoint-list' ? `${questionId}-1` : questionId],
+        });
 
-    const errorSummary = Selector('[data-test-id="error-summary"]');
-    const errorSummaryList = Selector('.nhsuk-error-summary__list');
-    const renderedQuestion = Selector(`[data-test-id="question-${questionId}"]`);
-    const submitButton = Selector('[data-test-id="section-submit-button"]');
-    const expectedErrorMessage = sectionManifest.questions[questionId].errorResponse.maxLength;
+      const errorSummary = Selector('[data-test-id="error-summary"]');
+      const errorSummaryList = Selector('.nhsuk-error-summary__list');
+      const renderedQuestion = Selector(`[data-test-id="question-${questionId}"]`);
+      const submitButton = Selector('[data-test-id="section-submit-button"]');
+      const expectedErrorMessage = sectionManifest.questions[questionId].errorResponse.maxLength;
 
-    await t
-      .expect(errorSummary.exists).notOk()
-      .click(submitButton.find('button'))
-      .expect(errorSummary.exists).ok()
-      .expect(errorSummaryList.find('li').count).eql(1)
-      .expect(errorSummaryList.find('li:nth-child(1)').innerText).eql(expectedErrorMessage)
-      .expect(errorSummaryList.find('li:nth-child(1) a').getAttribute('href')).eql(`#${questionType === 'bulletpoint-list' ? `${questionId}-1` : questionId}`)
-      .expect(renderedQuestion.find('.nhsuk-error-message').innerText).eql(`Error:\n${expectedErrorMessage}`);
-    if (questionType === 'textarea-field') {
       await t
-        .expect(renderedQuestion.find('.nhsuk-textarea--error').exists).ok();
-    }
-  });
+        .expect(errorSummary.exists).notOk()
+        .click(submitButton.find('button'))
+        .expect(errorSummary.exists).ok()
+        .expect(errorSummaryList.find('li').count).eql(1)
+        .expect(errorSummaryList.find('li:nth-child(1)').innerText).eql(expectedErrorMessage)
+        .expect(errorSummaryList.find('li:nth-child(1) a').getAttribute('href')).eql(`#${questionType === 'bulletpoint-list' ? `${questionId}-1` : questionId}`)
+        .expect(renderedQuestion.find('.nhsuk-error-message').innerText).eql(`Error:\n${expectedErrorMessage}`);
+      if (questionType === 'textarea-field') {
+        await t
+          .expect(renderedQuestion.find('.nhsuk-textarea--error').exists).ok();
+      }
+    });
+  }
 };
 
 const mandatoryErrorTest = ({
@@ -83,32 +86,35 @@ const mandatoryErrorTest = ({
   questionId,
   sectionApiUrl,
   apiLocalhost,
+  errorType,
 }) => {
-  test(`should show error summary and validation for ${questionId} question indicating it is mandatory`, async (t) => {
-    await pageSetup({ t });
-    nock(apiLocalhost)
-      .put(sectionApiUrl)
-      .reply(400, {
-        required: [questionId],
-      });
+  if (errorType === 'required') {
+    test(`should show error summary and validation for ${questionId} question indicating it is mandatory`, async (t) => {
+      await pageSetup({ t });
+      nock(apiLocalhost)
+        .put(sectionApiUrl)
+        .reply(400, {
+          required: [questionId],
+        });
 
-    const expectedQuestion = sectionManifest.questions[questionId];
-    const expectedErrorMessage = expectedQuestion.errorResponse.required;
-    const errorSummary = Selector('[data-test-id="error-summary"]');
-    const errorSummaryList = Selector('.nhsuk-error-summary__list');
-    const renderedQuestion = Selector(`[data-test-id="question-${questionId}"]`);
-    const submitButton = Selector('[data-test-id="section-submit-button"]');
+      const expectedQuestion = sectionManifest.questions[questionId];
+      const expectedErrorMessage = expectedQuestion.errorResponse.required;
+      const errorSummary = Selector('[data-test-id="error-summary"]');
+      const errorSummaryList = Selector('.nhsuk-error-summary__list');
+      const renderedQuestion = Selector(`[data-test-id="question-${questionId}"]`);
+      const submitButton = Selector('[data-test-id="section-submit-button"]');
 
-    await t
-      .expect(errorSummary.exists).notOk()
-      .click(submitButton.find('button'))
-      .expect(errorSummary.exists).ok()
-      .expect(errorSummaryList.find('li').count).eql(1)
-      .expect(errorSummaryList.find('li:nth-child(1)').innerText).eql(expectedErrorMessage)
-      .expect(errorSummaryList.find('li:nth-child(1) a').getAttribute('href')).eql(`#${questionId}`)
-      .expect(renderedQuestion.find(`[data-test-id="${expectedQuestion.type}-error"]`).exists).ok()
-      .expect(renderedQuestion.find('.nhsuk-error-message').innerText).eql(`Error:\n${expectedErrorMessage}`);
-  });
+      await t
+        .expect(errorSummary.exists).notOk()
+        .click(submitButton.find('button'))
+        .expect(errorSummary.exists).ok()
+        .expect(errorSummaryList.find('li').count).eql(1)
+        .expect(errorSummaryList.find('li:nth-child(1)').innerText).eql(expectedErrorMessage)
+        .expect(errorSummaryList.find('li:nth-child(1) a').getAttribute('href')).eql(`#${questionId}`)
+        .expect(renderedQuestion.find(`[data-test-id="${expectedQuestion.type}-error"]`).exists).ok()
+        .expect(renderedQuestion.find('.nhsuk-error-message').innerText).eql(`Error:\n${expectedErrorMessage}`);
+    });
+  }
 };
 
 export const runErrorTests = ({
@@ -119,27 +125,22 @@ export const runErrorTests = ({
   questionData,
   sectionId,
   apiLocalhost,
-  sectionParent,
 }) => {
   Object.keys(questionData.errorResponse).forEach((errorType) => {
-    if (errorType === 'required') {
-      mandatoryErrorTest({
-        pageSetup,
-        sectionManifest,
-        questionId,
-        sectionApiUrl,
-        apiLocalhost,
-      });
-    }
-    if (errorType === 'maxLength') {
-      maxLengthErrorTest({
-        pageSetup,
-        sectionManifest,
-        questionId,
-        sectionApiUrl,
-        apiLocalhost,
-      });
-    }
+    mandatoryErrorTest({
+      pageSetup,
+      sectionManifest,
+      questionId,
+      sectionApiUrl,
+      apiLocalhost,
+    });
+    maxLengthErrorTest({
+      pageSetup,
+      sectionManifest,
+      questionId,
+      sectionApiUrl,
+      apiLocalhost,
+    });
     goToAnchorFromErrorSummary({
       pageSetup,
       sectionManifest,
