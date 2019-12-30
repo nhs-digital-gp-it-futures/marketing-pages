45a87268a41d511daf8c17d8bbd41268e0bc7d7d
Ben.Hayward <ben.hayward@bjss.com>
2019-12-30 12:04:10 +0000

    Native mobile additional information form.

---
+++ app/section/sections/native-mobile/native-mobile-additional-information/ui.test.js
@@ -0,0 +1,14 @@
+import { runTestSuite } from '../../../../test-utils/runTestSuite';
+
+const sectionId = 'native-mobile-additional-information';
+const dashboardId = 'native-mobile';
+
+const additionalInformationData = {
+  'additional-information': 'It is possible that it may install on other platforms or versions not listed in this section. However, support is limited to systems that meet the minimum requirements.',
+};
+
+runTestSuite({
+  data: additionalInformationData,
+  sectionId,
+  dashboardId,
+});
