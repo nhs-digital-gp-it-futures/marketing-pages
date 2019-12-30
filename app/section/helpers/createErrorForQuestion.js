0ccaf585fc74ef7dd02dba364e983f80213c9184
alibombaye <alibombaye@gmail.com>
2019-12-30 13:35:04 +0000

    added function to handle new validation response

--- app/section/helpers/createErrorForQuestion.js
+++ app/section/helpers/createErrorForQuestion.js
@@ -17,3 +17,23 @@ export const createErrorForQuestion = ({
   }
   return undefined;
 };
+
+export const createErrorForQuestionNew = ({
+  questionId, questionManifest, validationErrors,
+}) => {
+  if (validationErrors && questionManifest && questionManifest.errorResponse) {
+    const errorForQuestion = Object.entries(validationErrors)
+      .reduce((errorForQuestionAcc, [erroredQuestion, errorType]) => {
+        if (erroredQuestion === questionId) {
+          return {
+            text: questionManifest.errorResponse[errorType],
+            href: `#${questionId}`,
+          };
+        }
+        return errorForQuestionAcc;
+      }, undefined);
+
+    return errorForQuestion;
+  }
+  return undefined;
+};
--- app/section/helpers/createErrorForQuestion.test.js
+++ app/section/helpers/createErrorForQuestion.test.js
@@ -1,4 +1,4 @@
-import { createErrorForQuestion } from './createErrorForQuestion';
+import { createErrorForQuestion, createErrorForQuestionNew } from './createErrorForQuestion';
 
 describe('createErrorForQuestion', () => {
   it('should return undefined if validationErrors is undefined', () => {
@@ -64,6 +64,77 @@ describe('createErrorForQuestion', () => {
     };
 
     const error = createErrorForQuestion({
+      questionId: 'some-question-id', questionManifest, validationErrors,
+    });
+
+    expect(error).toEqual(expectedError);
+  });
+});
+
+describe('createErrorForQuestionNew', () => {
+  it('should return undefined if validationErrors is undefined', () => {
+    const expectedError = undefined;
+
+    const questionManifest = undefined;
+
+    const validationErrors = undefined;
+
+    const error = createErrorForQuestionNew({
+      questionId: 'some-question-id', questionManifest, validationErrors,
+    });
+
+    expect(error).toEqual(expectedError);
+  });
+
+  it('should return undefined if questionManifest is undefined', () => {
+    const expectedError = undefined;
+
+    const questionManifest = undefined;
+
+    const validationErrors = {
+      'some-question-id': 'someErrorType',
+    };
+
+    const error = createErrorForQuestionNew({
+      questionId: 'some-question-id', questionManifest, validationErrors,
+    });
+
+    expect(error).toEqual(expectedError);
+  });
+
+  it('should return undefined if there is no errorResponse in the questionManifest', () => {
+    const expectedError = undefined;
+
+    const questionManifest = {};
+
+    const validationErrors = {
+      'some-question-id': 'someErrorType',
+    };
+
+    const error = createErrorForQuestionNew({
+      questionId: 'some-question-id', questionManifest, validationErrors,
+    });
+
+    expect(error).toEqual(expectedError);
+  });
+
+  it('should return generated error for the question', () => {
+    const expectedError = {
+      text: 'some error message',
+      href: '#some-question-id',
+    };
+
+    const questionManifest = {
+      errorResponse: {
+        someErrorType: 'some error message',
+      },
+    };
+
+    const validationErrors = {
+      'some-question-id': 'someErrorType',
+    };
+
+    const error = createErrorForQuestionNew({
       questionId: 'some-question-id', questionManifest, validationErrors,
     });
 
