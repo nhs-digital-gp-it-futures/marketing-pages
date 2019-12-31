d2f53743c98f5ae1da2e05412c55565cfccb8236
alibombaye <alibombaye@gmail.com>
2019-12-31 14:50:37 +0000

    updated ui-test helpers to handle multi-question type

--- app/test-utils/testFunctions/questionTests/errorTests.js
+++ app/test-utils/testFunctions/questionTests/errorTests.js
@@ -11,6 +11,7 @@ const goToAnchorFromErrorSummary = ({
   errorType,
   sectionManifest,
   dashboardId,
+  parentQuestionId,
 }) => {
   test(`should go to anchor when clicking the ${questionId} summary error link`, async (t) => {
     await pageSetup({ t });
@@ -18,30 +19,39 @@ const goToAnchorFromErrorSummary = ({
 
     // bulletpoint-list numbers the input fields so it is not just the question id
     const questionIdBasedOnType = questionType === 'bulletpoint-list' ? `${questionId}-1` : questionId;
+    const questionIdBasedOnParentId = parentQuestionId ? `${parentQuestionId}[${questionIdBasedOnType}]` : questionIdBasedOnType;
+    const responseBodyBasedOnType = parentQuestionId
+      ? {
+        [parentQuestionId]: {
+          [questionIdBasedOnType]: errorType,
+        },
+      }
+      : {
+        [questionIdBasedOnType]: errorType,
+      };
 
     nock(apiLocalhost)
       .put(`${apiPath}/sections/${sectionId}`)
-      .reply(400, {
-        [questionIdBasedOnType]: errorType,
-      });
+      .reply(400, responseBodyBasedOnType);
 
     const errorSummary = Selector('[data-test-id="error-summary"]');
     const errorSummaryList = Selector('.nhsuk-error-summary__list');
     const submitButton = Selector('[data-test-id="section-submit-button"]');
+    const expectedAnchorLink = `#${questionIdBasedOnParentId}`;
 
     await t
       .expect(errorSummary.exists).notOk()
       .click(submitButton.find('button'))
       .expect(errorSummary.exists).ok()
       .expect(errorSummaryList.find('li:nth-child(1) a').count).eql(1)
-      .expect(errorSummaryList.find('li:nth-child(1) a').getAttribute('href')).eql(`#${questionIdBasedOnType}`)
+      .expect(errorSummaryList.find('li:nth-child(1) a').getAttribute('href')).eql(expectedAnchorLink)
       .click(errorSummaryList.find('li:nth-child(1) a'));
     if (dashboardId) {
       await t
-        .expect(getLocation()).contains(`/solution/S100000-001/dashboard/${dashboardId}/section/${sectionId}#${questionIdBasedOnType}`);
+        .expect(getLocation()).contains(`/solution/S100000-001/dashboard/${dashboardId}/section/${sectionId}#${questionIdBasedOnParentId}`);
     } else {
       await t
-        .expect(getLocation()).contains(`/solution/S100000-001/section/${sectionId}#${questionIdBasedOnType}`);
+        .expect(getLocation()).contains(`/solution/S100000-001/section/${sectionId}#${questionIdBasedOnParentId}`);
     }
   });
 };
@@ -52,26 +62,41 @@ const maxLengthErrorTest = ({
   questionId,
   errorType,
   sectionId,
+  parentQuestionId,
 }) => {
   if (errorType === 'maxLength') {
     test(`should show error summary and validation for ${questionId} question when it exceeds the maxLength`, async (t) => {
-      const questionType = sectionManifest.questions[questionId].type;
       await pageSetup({ t });
+      const questionType = sectionManifest.questions[questionId].type;
 
       // bulletpoint-list numbers the input fields so it is not just the question id
       const questionIdBasedOnType = questionType === 'bulletpoint-list' ? `${questionId}-1` : questionId;
+      const questionIdBasedOnParentId = parentQuestionId ? `${parentQuestionId}[${questionIdBasedOnType}]` : questionIdBasedOnType;
+      const responseBodyBasedOnType = parentQuestionId
+        ? {
+          [parentQuestionId]: {
+            [questionIdBasedOnType]: 'maxLength',
+          },
+        }
+        : {
+          [questionIdBasedOnType]: 'maxLength',
+        };
 
       nock(apiLocalhost)
         .put(`${apiPath}/sections/${sectionId}`)
-        .reply(400, {
-          [questionIdBasedOnType]: 'maxLength',
-        });
+        .reply(400, responseBodyBasedOnType);
 
       const errorSummary = Selector('[data-test-id="error-summary"]');
       const errorSummaryList = Selector('.nhsuk-error-summary__list');
-      const renderedQuestion = Selector(`[data-test-id="question-${questionId}"]`);
+
+      const renderedQuestionSelector = questionType === 'bulletpoint-list'
+        ? `field-error-${questionIdBasedOnParentId}`
+        : `question-${questionIdBasedOnParentId}`;
+
+      const renderedQuestion = Selector(`[data-test-id="${renderedQuestionSelector}"]`);
       const submitButton = Selector('[data-test-id="section-submit-button"]');
       const expectedErrorMessage = sectionManifest.questions[questionId].errorResponse.maxLength;
+      const expectedAnchorLink = `#${questionIdBasedOnParentId}`;
 
       await t
         .expect(errorSummary.exists).notOk()
@@ -79,7 +104,7 @@ const maxLengthErrorTest = ({
         .expect(errorSummary.exists).ok()
         .expect(errorSummaryList.find('li').count).eql(1)
         .expect(errorSummaryList.find('li:nth-child(1)').innerText).eql(expectedErrorMessage)
-        .expect(errorSummaryList.find('li:nth-child(1) a').getAttribute('href')).eql(`#${questionType === 'bulletpoint-list' ? `${questionId}-1` : questionId}`)
+        .expect(errorSummaryList.find('li:nth-child(1) a').getAttribute('href')).eql(expectedAnchorLink)
         .expect(renderedQuestion.find('.nhsuk-error-message').innerText).eql(`Error:\n${expectedErrorMessage}`);
       if (questionType === 'textarea-field') {
         await t
@@ -134,6 +159,7 @@ export const runErrorTests = ({
   sectionManifest,
   questionId,
   sectionId,
+  sectionParent,
   questionData,
   dashboardId,
 }) => {
@@ -151,6 +177,7 @@ export const runErrorTests = ({
       questionId,
       sectionId,
       errorType,
+      parentQuestionId: sectionParent,
     });
     goToAnchorFromErrorSummary({
       pageSetup,
@@ -159,6 +186,7 @@ export const runErrorTests = ({
       sectionId,
       errorType,
       dashboardId,
+      parentQuestionId: sectionParent,
     });
   });
 };
