cb8c8eb96dd7b989841f17ee015d8ffe02893867
alibombaye <alibombaye@gmail.com>
2020-01-17 19:46:30 +0000

    Added controller test for subDashboards

---
+++ app/pages/dashboard/subDashboards/controller.test.js
@@ -0,0 +1,65 @@
+import { getSubDashboardPageContext } from './controller';
+import { ManifestProvider } from '../../../manifestProvider';
+import { ApiProvider } from '../../../apiProvider';
+
+jest.mock('../../../manifestProvider');
+jest.mock('../../../apiProvider');
+
+describe('subDashboards controller', () => {
+  const dashboardManifest = {
+    sectionGroups: {
+      'some-section-group-id': {
+        title: 'Some section group',
+        sections: {
+          'some-section-id': {
+            title: 'Some section',
+            type: 'section',
+          },
+        },
+      },
+    },
+  };
+
+  const marketingDataSections = {
+    data: {
+      sections: {
+        'some-section-id': {
+          status: 'INCOMPLETE',
+          requirement: 'Mandatory',
+        },
+      },
+    },
+  };
+
+  it('should return the context when the manifest and data from the api is provided', async () => {
+    const expectedContext = {
+      previewUrl: '/solution/some-solution-id/preview',
+      submitForModerationUrl: '/solution/some-solution-id/submitForModeration',
+      returnToDashboardUrl: '/solution/some-solution-id',
+      sectionGroups: [
+        {
+          id: 'some-section-group-id',
+          title: 'Some section group',
+          sections: [
+            {
+              URL: '/solution/some-solution-id/dashboard/some-dashboard-id/section/some-section-id',
+              id: 'some-section-id',
+              title: 'Some section',
+              requirement: 'Mandatory',
+              status: 'INCOMPLETE',
+              isActive: true,
+            },
+          ],
+        },
+      ],
+    };
+
+    ManifestProvider.prototype.getSubDashboardManifest.mockReturnValue(dashboardManifest);
+
+    ApiProvider.prototype.getSubDashboardData.mockResolvedValue(marketingDataSections);
+
+    const context = await getSubDashboardPageContext({ solutionId: 'some-solution-id', dashboardId: 'some-dashboard-id' });
+
+    expect(context).toEqual(expectedContext);
+  });
+});
