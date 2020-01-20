bab8a78867696107ed8294bca07836380b0e762f
alibombaye <alibombaye@gmail.com>
2020-01-20 18:43:34 +0000

    added unit test for getSectionPageErrorContext

--- app/pages/section/controller.test.js
+++ app/pages/section/controller.test.js
@@ -1,4 +1,4 @@
-import { getSectionPageContext } from './controller';
+import { getSectionPageContext, getSectionPageErrorContext } from './controller';
 import { ManifestProvider } from '../../manifestProvider';
 import { ApiProvider } from '../../apiProvider';
 
@@ -6,26 +6,27 @@ jest.mock('../../manifestProvider');
 jest.mock('../../apiProvider');
 
 describe('section controller', () => {
+  const sectionManifest = {
+    id: 'some-section-id',
+    title: 'Some section title',
+    mainAdvice: 'Some main advice.',
+    additionalAdvice: [
+      'Some first bit of additional advice.',
+      'Some second bit of additional advice.',
+      'Some third bit of additional advice.',
+    ],
+    questions: {
+      'some-question-id': {},
+    },
+    submitText: 'some-submit-text',
+  };
+
+  const sectionData = {
+    data: {},
+  };
+
+
   describe('getSectionPageContext', () => {
-    const sectionManifest = {
-      id: 'some-section-id',
-      title: 'Some section title',
-      mainAdvice: 'Some main advice.',
-      additionalAdvice: [
-        'Some first bit of additional advice.',
-        'Some second bit of additional advice.',
-        'Some third bit of additional advice.',
-      ],
-      questions: {
-        'some-question-id': {},
-      },
-      submitText: 'some-submit-text',
-    };
-
-    const sectionData = {
-      data: {},
-    };
-
     it('should return the context when the manifest and data from the api is provided', async () => {
       const expectedContext = {
         title: 'Some section title',
@@ -62,6 +63,35 @@ describe('section controller', () => {
       } catch (err) {
         expect(err).toEqual(new Error('No data returned'));
       }
+    });
+  });
+
+  describe('getSectionPageErrorContext', () => {
+    it('should return the context when the manifest is provided', async () => {
+      const expectedContext = {
+        title: 'Some section title',
+        submitActionUrl: '/solution/some-solution-id/section/some-section-id',
+        mainAdvice: 'Some main advice.',
+        additionalAdvice: [
+          'Some first bit of additional advice.',
+          'Some second bit of additional advice.',
+          'Some third bit of additional advice.',
+        ],
+        questions: [
+          {
+            id: 'some-question-id',
+          },
+        ],
+        returnToDashboardUrl: '/solution/some-solution-id',
+        submitText: 'some-submit-text',
+      };
+
+      ManifestProvider.prototype.getSectionManifest.mockReturnValue(sectionManifest);
+      ApiProvider.prototype.getSectionData.mockResolvedValue(sectionData);
+
+      const context = await getSectionPageErrorContext({ solutionId: 'some-solution-id' });
+
+      expect(context).toEqual(expectedContext);
     });
   });
 });
