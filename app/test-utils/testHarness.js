a6c41e7f3bb996482c5ea884232a3d6e683f4542
Ben.Hayward <ben.hayward@bjss.com>
2020-02-06 13:16:24 +0000

    Removed commented out code

--- app/test-utils/testHarness.js
+++ app/test-utils/testHarness.js
@@ -38,30 +38,3 @@ export const createTestHarness = (setup, callback) => (done) => {
     done,
   }));
 };
-
-// import express from 'express';
-// import nunjucks from 'nunjucks';
-// import { App } from '../../app';
-//
-// export const testHarness = () => {
-//   const app = new App().createApp();
-//   const router = express.Router();
-//
-//   return {
-//     createComponentDummyApp(template, context) {
-//       const dummyRouter = router.get('/', (req, res) => {
-//         res.render(template, context);
-//       });
-//       app.use(dummyRouter);
-//       return app;
-//     },
-//     createTemplateDummyApp(template, context) {
-//       const dummyRouter = router.get('/', (req, res) => {
-//         const viewToTest = nunjucks.renderString(template, context);
-//         res.send(viewToTest);
-//       });
-//       app.use(dummyRouter);
-//       return app;
-//     },
-//   };
-// };
