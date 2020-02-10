c963b1d3c25d095d6e796cf1c9a4b0d1a4d5fe6d
kat1906 <katie.armstrong@bjss.com>
2020-02-10 17:56:08 +0000

    add integrations to preview

--- app/pages/preview/template.test.js
+++ app/pages/preview/template.test.js
@@ -39,6 +39,42 @@ describe('preview page', () => {
     });
   }));
 
+  describe('integrations', () => {
+    it('should render the integrations section if link is provided', createTestHarness(setup, (harness) => {
+      const context = {
+        sections: {
+          integrations: {
+            answers: {
+              link: 'link.com',
+            },
+          },
+        },
+      };
+
+      harness.request(context, ($) => {
+        expect($('[data-test-id="view-integrations"]').length).toEqual(1);
+        expect($('[data-test-id="view-question-data-text-link"]').length).toEqual(1);
+      });
+    }));
+
+    it('should render the integrations section document-name is provided', createTestHarness(setup, (harness) => {
+      const context = {
+        sections: {
+          integrations: {
+            answers: {
+              'document-name': 'document.pdf',
+            },
+          },
+        },
+      };
+
+      harness.request(context, ($) => {
+        expect($('[data-test-id="view-integrations"]').length).toEqual(1);
+        expect($('[data-test-id="view-question-data-text-link-authority-integrations"]').length).toEqual(1);
+      });
+    }));
+  });
+
   it('should render the client application types section when provided', createTestHarness(setup, (harness) => {
     const context = {
       sections: {
