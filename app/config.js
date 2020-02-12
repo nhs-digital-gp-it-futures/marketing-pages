70aa151c8b8ff2c3312f7c0d343b557028411a56
Tony Lawson <55191681+talawson05@users.noreply.github.com>
2020-02-12 13:15:17 +0000

    changed default config for documentHost to 8090 (#142)

--- app/config.js
+++ app/config.js
@@ -12,7 +12,7 @@ module.exports = {
   apiHost: process.env.API_HOST || 'http://localhost:8080',
 
   // DOCUMENT_HOST
-  documentHost: process.env.DOCUMENT_HOST || 'http://localhost:8081',
+  documentHost: process.env.DOCUMENT_HOST || 'http://localhost:8090',
 
   // LOGGER_LEVEL options are info, warn, error, off
   loggerLevel: process.env.LOGGER_LEVEL || 'error',
