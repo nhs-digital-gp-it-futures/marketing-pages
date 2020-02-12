b48b8881b04f5701b308641a21ea07698a3ddc29
kat1906 <katie.armstrong@bjss.com>
2020-02-12 14:34:02 +0000

    replave preview with viewSolution component

--- app/pages/preview/template.test.js
+++ app/pages/preview/template.test.js
@@ -7,7 +7,7 @@ const setup = {
 };
 
 describe('preview page', () => {
-  it('should render the title of the preview page', createTestHarness(setup, (harness) => {
+  it('should render the viwSolution component', createTestHarness(setup, (harness) => {
     const context = {
       solutionHeader: {
         id: '100000-001',
@@ -21,111 +21,6 @@ describe('preview page', () => {
 
     harness.request(context, ($) => {
       expect($('h1').text().trim()).toEqual('Write on Time');
-    });
-  }));
-
-  it('should render the solutions-description section when provided', createTestHarness(setup, (harness) => {
-    const context = {
-      sections: {
-        'solution-description': {},
-      },
-    };
-
-    harness.request(context, ($) => {
-      expect($('[data-test-id="view-solution-description"]').length).toEqual(1);
-    });
-  }));
-
-  it('should render the features section when provided', createTestHarness(setup, (harness) => {
-    const context = {
-      sections: {
-        features: {},
-      },
-    };
-
-    harness.request(context, ($) => {
-      expect($('[data-test-id="view-features"]').length).toEqual(1);
-    });
-  }));
-
-  describe('integrations', () => {
-    it('should render the integrations section if link is provided', createTestHarness(setup, (harness) => {
-      const context = {
-        sections: {
-          integrations: {
-            answers: {
-              link: 'link.com',
-            },
-          },
-        },
-      };
-
-      harness.request(context, ($) => {
-        expect($('[data-test-id="view-integrations"]').length).toEqual(1);
-        expect($('[data-test-id="view-question-data-text-link-supplier-integrations"]').length).toEqual(1);
-      });
-    }));
-
-    it('should render the integrations section document-link is provided', createTestHarness(setup, (harness) => {
-      const context = {
-        sections: {
-          integrations: {
-            answers: {
-              'document-link': 'document.pdf',
-            },
-          },
-        },
-      };
-
-      harness.request(context, ($) => {
-        expect($('[data-test-id="view-integrations"]').length).toEqual(1);
-        expect($('[data-test-id="view-question-data-text-link-authority-integrations"]').length).toEqual(1);
-      });
-    }));
-  });
-
-  it('should render the client application types section when provided', createTestHarness(setup, (harness) => {
-    const context = {
-      sections: {
-        'client-application-types': {},
-      },
-    };
-
-    harness.request(context, ($) => {
-      expect($('[data-test-id="view-client-application-types"]').length).toEqual(1);
-    });
-  }));
-
-  it('should render the hosting types section when one of the sections is provided', createTestHarness(setup, (harness) => {
-    const context = {
-      sections: {
-        'hosting-type-private-cloud': {},
-      },
-    };
-
-    harness.request(context, ($) => {
-      expect($('[data-test-id="view-hosting-types"]').length).toEqual(1);
-    });
-  }));
-
-  it('should render the capabilities section when provided', createTestHarness(setup, (harness) => {
-    const context = {
-      sections: {
-        capabilities: {
-          answers: {
-            'capabilities-met': [{
-              name: 'Communicate With Practice - Citizen',
-              version: '1.0.1',
-              description: 'Supports secure and trusted electronic communications between Citizens and the Practice. Integrates with Patient Information Maintenance.',
-              link: 'https://gpitbjss.atlassian.net/wiki/spaces/GPITF/pages/1391134188/Communicate+With+Practice+-+Citizen',
-            }],
-          },
-        },
-      },
-    };
-
-    harness.request(context, ($) => {
-      expect($('[data-test-id="view-capabilities"]').length).toEqual(1);
     });
   }));
 });
