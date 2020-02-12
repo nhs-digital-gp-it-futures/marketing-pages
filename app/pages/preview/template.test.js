35280ce8ef2127d0f97531520654812e10675a4b
kat1906 <katie.armstrong@bjss.com>
2020-02-12 15:27:10 +0000

    add backLink to preview

--- app/pages/preview/template.test.js
+++ app/pages/preview/template.test.js
@@ -7,7 +7,19 @@ const setup = {
 };
 
 describe('preview page', () => {
-  it('should render the viwSolution component', createTestHarness(setup, (harness) => {
+  it('should render the backLink component', createTestHarness(setup, (harness) => {
+    const context = {
+      returnToDashboardUrl: '/supplier/solution/100000-001',
+    };
+
+    harness.request(context, ($) => {
+      const backLink = $('[data-test-id="view-solution-page-back-link"] a');
+      expect(backLink.length).toEqual(1);
+      expect(backLink.text().trim()).toEqual('Go back to previous page');
+    });
+  }));
+
+  it('should render the viewSolution component', createTestHarness(setup, (harness) => {
     const context = {
       solutionHeader: {
         id: '100000-001',
