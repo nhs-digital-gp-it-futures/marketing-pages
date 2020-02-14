3d6912b97b1b3fbbe5e6b5cb115584e3a9f98a55
Ben.Hayward <ben.hayward@bjss.com>
2020-02-14 16:04:39 +0000

    Revert testcafeRunner.js

--- testcafeRunner.js
+++ testcafeRunner.js
@@ -9,14 +9,14 @@ app.use('/', routes);
 const server = app.listen('1234');
 
 const browserFromArgs = process.argv.slice(2, 3);
-const browserToRun = browserFromArgs.length > 0 ? browserFromArgs : 'chrome';
+const browserToRun = browserFromArgs.length > 0 ? browserFromArgs : 'chrome:headless';
 
 const testFromArgs = process.argv.slice(3, 4);
 const testsToRun = testFromArgs ? `**/*${testFromArgs}*/ui.test.js` : '**/*ui.test.js';
 
-let concurrency = 1;
-let stopOnFirstFail = false;
-let quarantineMode = false;
+let concurrency = 4;
+let stopOnFirstFail = true;
+let quarantineMode = true;
 if (env === 'pipeline' || browserFromArgs.length > 0) {
   concurrency = 1;
   stopOnFirstFail = false;
