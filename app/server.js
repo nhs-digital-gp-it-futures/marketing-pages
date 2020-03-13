16d0251b60c36a368000c4f48e2dd333bdaf482a
kat1906 <katie.armstrong@bjss.com>
2020-03-13 15:20:00 +0000

    improve link

--- app/server.js
+++ app/server.js
@@ -16,7 +16,7 @@ Object.keys(config).map((configKey) => {
 const app = new App().createApp();
 app.use('/', routes);
 if (config.env === 'development') {
-  logger.info(` Marketing Pages - \x1b[35m${config.appBaseUri}\x1b[0m`);
+  logger.info(` Marketing Pages - \x1b[35m${config.appBaseUri}/supplier/solution/100000-001\x1b[0m`);
 } else {
   logger.info(`App listening on port ${config.port} - Marketing Pages`);
 }
